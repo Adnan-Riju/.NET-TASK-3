f87ae42b3b92ed6fff6a8c9e7289d1a94f9dd4d7
unknown <adnanriju25@gmail.com>
2023-10-20 00:16:52 +0600

    project

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using AR_Shopping.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AR_Shopping.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Customer_Dashboard()
+        {
+            var db = new ProjectEntities2();
+            var data = db.tbl_Product.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Product_Details()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult  Product_Details(tbl_Product i)
+        {
+
+            var db = new ProjectEntities2();
+            var data2 = db.tbl_Product.ToList();
+            return View(data2);
+        }
+
+        }
+}
+  
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using AR_Shopping.EF;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AR_Shopping.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ProjectEntities2();
+            var data = db.SignUps.ToList();
+            return View(data);
+
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUp s)
+        {
+
+            if (ModelState.IsValid)
+            {  
+                var db = new ProjectEntities2();
+                db.SignUps.Add(s);
+                db.SaveChanges();
+                return View("Login");
+                
+            }
+            return RedirectToAction("SignUp");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login objuser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ProjectEntities2 db = new ProjectEntities2())
+                {
+                    var obj = db.SignUps.Where(a => a.UserName.Equals(objuser.UserName) && a.Password.Equals(objuser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                       // Session["UserID"] = obj.UserName.ToString();
+                       // Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("Dashboard");
+                    }
+                   
+                }
+            }
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(SignUp I)
+        {
+            var db = new ProjectEntities2();
+            db.SignUps.Add(I);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ProjectEntities2();
+            var data = db.SignUps.Find(id);
+           
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(SignUp d)
+        {
+            var db = new ProjectEntities2();
+            var ex = db.SignUps.Find(d.Id);
+            ex.Name = d.Name;
+            db.SaveChanges();
+            TempData["AllertMessage"] = "product updated";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new ProjectEntities2();
+            var name = (from b in db.SignUps
+                        where b.Id == id
+                        select b).SingleOrDefault();
+            db.SignUps.Remove(name);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
